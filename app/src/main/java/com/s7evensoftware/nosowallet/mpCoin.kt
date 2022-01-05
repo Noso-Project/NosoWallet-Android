b8e53009399eff39c34428f0825ab41d16dd1fc8
Stevengez <steven.jsg@gmail.com>
2022-01-04 21:40:12 -0600

    Unused method
    Removed unused method from mpCoin

--- app/src/main/java/com/s7evensoftware/nosowallet/mpCoin.kt
+++ app/src/main/java/com/s7evensoftware/nosowallet/mpCoin.kt
@@ -19,23 +19,5 @@ class mpCoin {
             val result = balancStr.substring(0, balancStr.length-8) + "." + balancStr.substring(balancStr.length-8)
             return if(balance >= 0) result else "-"+result
         }
-
-        fun isCoinFormat(amount:String):Boolean{
-            var dotpoint = 0
-            var chars_after_point = 0
-            for((index, char) in amount.withIndex()){
-                if(dotpoint > 0){
-                    chars_after_point++
-                }
-                if(char == '.'){
-                    dotpoint = index
-                }
-            }
-
-            if(chars_after_point == 8){
-                return true
-            }
-            return false
-        }
     }
 }
