78ae4c6900c26e1e4d56868ce6643c1ed45c04f4
Stevengez <steven.jsg@gmail.com>
2022-01-14 23:54:19 -0600

    Log Size reduced
    Reduced the log report size to 50KB to make it easier to handle

--- app/src/main/java/com/s7evensoftware/nosowallet/mpdisk.kt
+++ app/src/main/java/com/s7evensoftware/nosowallet/mpdisk.kt
@@ -304,7 +304,7 @@ class mpDisk {
                     fileLog.parentFile.mkdirs()
                     fileLog.createNewFile()
                 }else{
-                    if(fileLog.length() >= 1048576){
+                    if(fileLog.length() >= 51200){
                         if(fileLog.delete()){
                             Log.e("mpDisk","Log file cleared - OK")
                             fileLog.createNewFile()
